60051600708acb53c2429d041a7aa97e6bb5246a
Kingfish404 <jinyu929@qq.com>
2020-06-02 19:11:26 +0800

    update

--- src/LiveAndDeath/Main.java
+++ src/LiveAndDeath/Main.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // 输入概率
         System.out.println("请输入每个细胞死亡的概率");
         java.util.Scanner scan  =new Scanner(System.in);
         double i;
         i = scan.nextDouble();
         scan.close();
+
+        // 生成对象
         TimeCtrl timeCtrl = new TimeCtrl(i);
+
+        // 开始运行
         timeCtrl.run();
     }
 }
